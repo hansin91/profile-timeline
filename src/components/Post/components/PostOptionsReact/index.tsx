a0fe6108eccf9bec0ec263a82660941d42a3d6e9
hansin91 <hansinsusatya@gmail.com>
2020-08-25 11:22:31 +0700

    React Click

--- src/components/Post/components/PostOptionsReact/index.tsx
+++ src/components/Post/components/PostOptionsReact/index.tsx
@@ -1,25 +1,26 @@
 import './PostOptionsReact.scss'
-import React, {useState} from 'react'
+import React from 'react'
 import {reactionOptions} from '../../../../data/reaction-options'
 import { Reaction } from '../../../../types'
 import PostOptionReact from '../PostOptionReact'
 import useVisible from '../../../../hooks/useVisible'
 
 function PostOptionsReact() {
-  const [show, setShow] = useState(false)
   const { ref, isVisible, setIsVisible } = useVisible(false);
   return(
-    <div onClickCapture={() => setIsVisible(!isVisible)} className="post-option-wrap" style={{position: 'relative'}}>
-      <div className="post-option reaction-options-dropdown-trigger">
-        <svg className="post-option-icon icon-thumbs-up">
-          <use xlinkHref="#svg-thumbs-up"></use>
-        </svg>
-        <p className="post-option-text">React!</p>
-      </div>
-      <div ref={ref} className={`reaction-options reaction-options-dropdown `+(isVisible ? 'show' : '')}>
-        {reactionOptions.map((option: Reaction, index) =>
-          <PostOptionReact key={index} option={option} />
-        )}
+    <div ref={ref}>
+      <div onClickCapture={() => setIsVisible(!isVisible)} className="post-option-wrap" style={{position: 'relative'}}>
+        <div className="post-option reaction-options-dropdown-trigger">
+          <svg className="post-option-icon icon-thumbs-up">
+            <use xlinkHref="#svg-thumbs-up"></use>
+          </svg>
+          <p className="post-option-text">React!</p>
+        </div>
+        <div className={`reaction-options reaction-options-dropdown `+(isVisible ? 'show' : '')}>
+          {reactionOptions.map((option: Reaction, index) =>
+            <PostOptionReact key={index} option={option} />
+          )}
+        </div>
       </div>
     </div>
   )
