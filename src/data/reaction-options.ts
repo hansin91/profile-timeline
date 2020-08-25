45f69b205c9d94d70fa9ab371bce59dd99cbf3f2
hansin91 <hansinsusatya@gmail.com>
2020-08-25 09:43:10 +0700

    Options React

--- src/data/reaction-options.ts
+++ src/data/reaction-options.ts
@@ -6,5 +6,29 @@ export const reactionOptions = [
   {
     name: 'Love',
     icon: 'love'
+  },
+  {
+    name: 'Dislike',
+    icon: 'dislike'
+  },
+  {
+    name: 'Happy',
+    icon: 'happy'
+  },
+  {
+    name: 'Funny',
+    icon: 'funny'
+  },
+  {
+    name: 'Wow',
+    icon: 'wow'
+  },
+  {
+    name: 'Angry',
+    icon: 'angry'
+  },
+  {
+    name: 'Sad',
+    icon: 'sad'
   }
 ]
